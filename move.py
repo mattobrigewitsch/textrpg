d0ca3d031c7edf5fab1e54b1619854e10c53770c
mattobrigewitsch <54864684+mattobrigewitsch@users.noreply.github.com>
2019-11-06 21:22:54 -0600

    added to move.py
    apart from additions to the map and new events, a foundational version of move.py is complete

--- move.py
+++ move.py
@@ -1,10 +1,9 @@
-
 #To add a layer to the map, draw another layer using the same format,
 #and reference in yaxislist. These are the only 2 references necessary.
 yaxis0 = [' . ',' . ',' . ',' . ',' . ',' . ']
-yaxis1 = [' . ',' . ',' . ',' . ',' . ',' . ',' . ',' X ']
+yaxis1 = [' . ',' . ',' . ',' . ',' . ',' . ',' . ',' . ']
 yaxis2 = [' . ',' . ',' . ',' . ',' . ',' . ',' . ']
-yaxis3 = [' . ',' . ',' . ',' . ',' . ',' . ',' . ',' . ']
+yaxis3 = [' . ',' X ',' . ',' . ',' o ',' . ',' . ',' . ']
 yaxis4 = [' . ',' . ',' . ',' . ',' . ',' . ']
 
 
@@ -42,7 +41,21 @@ def worlddisplay(minyaxisnum):
         interval = interval + 1
     print("[][][][][][][][]")
 
-#def eventfinder(eventcode):
+def eventfinder(eventcode):
+    if eventcode == ' . ':
+        #this would be a nested function if I was allowed to use them
+        if "w" in userin or "s" in userin:
+            tempyaxis.insert(xaxis, ' X ')
+            del activeyaxis[xaxis]
+        activeyaxis.insert(xaxis, oldev)
+        worlddisplay(minyaxis)
+    if eventcode == ' o ':
+        if "w" in userin or "s" in userin:
+            tempyaxis.insert(xaxis, ' X ')
+            del activeyaxis[xaxis]
+        activeyaxis.insert(xaxis, oldev)
+        worlddisplay(minyaxis)
+        print("you walked over the amazing test rock! congratulations!")
 
 worlddisplay(minyaxis)
 
@@ -58,27 +71,21 @@ while userin != "quit":
         print("xaxis: " + str(xaxis))
     if userin == "a":
         if xaxis != 0:
-            if activeyaxis[xaxis - 1] == ' . ':
-                del activeyaxis[xaxis - 1]
-                activeyaxis.insert(xaxis + 1, ' . ')
-                worlddisplay(minyaxis)
+            oldev = ev
+            ev = activeyaxis.pop(xaxis - 1)
+            eventfinder(ev)
     elif userin == "d":
         if xaxis != len(activeyaxis)-1:
-            if activeyaxis[xaxis + 1] == ' . ':
-                activeyaxis.insert(xaxis, ' . ')
-                del activeyaxis[xaxis + 2]
-                worlddisplay(minyaxis)
+            oldev = ev
+            ev = activeyaxis.pop(xaxis + 1)
+            eventfinder(ev)
     elif userin == "w":
         if activeyaxis != yaxis0:
             try:
                 tempyaxis = yaxislist[yaxis - 1]
                 oldev = ev
                 ev = tempyaxis.pop(xaxis)
-                if ev == ' . ':
-                    del activeyaxis[xaxis]
-                    activeyaxis.insert(xaxis, oldev)
-                    tempyaxis.insert(xaxis, ' X ')
-                    worlddisplay(minyaxis)
+                eventfinder(ev)
             except:
                 print("There is nothing in that direction")
     elif userin == "s":
@@ -87,10 +94,6 @@ while userin != "quit":
                 tempyaxis = yaxislist[yaxis + 1]
                 oldev = ev
                 ev = tempyaxis.pop(xaxis)
-                if ev == ' . ':
-                    del activeyaxis[xaxis]
-                    activeyaxis.insert(xaxis, oldev)
-                    tempyaxis.insert(xaxis, ' X ')
-                    worlddisplay(minyaxis)
+                eventfinder(ev)
             except:
                 print("There is nothing in that direction")
